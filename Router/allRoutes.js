b12efe1e4e7456e29fa30f8b7140b1b51ef45989
Elodie <elodie.devweb@gmail.com>
2025-11-02 17:44:29 +0100

    Galerie page created

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,9 @@ import Route from "./Route.js";
 
 //Routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "Galerie", "/pages/galerie.html"),
+];
 
 //title : Route.title - websitename
 export const websiteName = "Quai Antique";
